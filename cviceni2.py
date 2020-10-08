31342131f8b355b92270b7d78f3a14a52d371694
Vít Šefl <vituscze@gmail.com>
2020-10-08 16:18:48 +0200

    Update cviceni2.py

--- cviceni2.py
+++ cviceni2.py
@@ -1,3 +1,46 @@
+x = [int(r) for r in input().split()]
+y = int(input())
+
+low = 0
+high = len(x)
+found = False
+while not found and high > low:
+    mid = low + (high - low) // 2
+    if y > x[mid]:
+        low = mid + 1
+    elif y < x[mid]:
+        high = mid
+    else:
+        found = True
+
+if found:
+    print('Found!')
+else:
+    print('Too bad')
+
+# ----------
+
+x = [int(r) for r in input().split()]
+
+if len(x) % 2 == 1:
+    x.append(x[-1])
+
+if len(x) > 0:
+    for i in range(0, len(x), 2):
+        if x[i] > x[i + 1]:
+            x[i], x[i + 1] = x[i + 1], x[i]
+    maybeMin = x[0]
+    maybeMax = x[1]
+    for i in range(2, len(x), 2):
+        if x[i] < maybeMin:
+            maybeMin = x[i]
+        if x[i + 1] > maybeMax:
+            maybeMax = x[i + 1]
+    print('min', maybeMin)
+    print('max', maybeMax)
+
+# ----------
+
 def factorial(n):
     r = 1
     for i in range(1, n + 1):
@@ -14,9 +57,3 @@ def fib(n):
         for i in range(n - 1):
             a, b = a + b, a
         return a
-
-def palindrom(s):
-    for i in range(len(s) // 2):
-        if s[i] != s[-1 - i]:
-            return False
-    return True
