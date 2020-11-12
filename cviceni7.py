6e2f2cb8664e9a755ab650c2ac4ee35e207c9d19
Vít Šefl <vituscze@gmail.com>
2020-11-12 16:08:58 +0100

    Update cviceni7.py

--- cviceni7.py
+++ cviceni7.py
@@ -1,42 +1,158 @@
-# hanoi(4, 'left', 'middle', 'right')
-def hanoi(n, start, end, via):
-    if n == 1:
-        print('Move', start, 'to', end)
-    else:
-        hanoi(n - 1, start, via, end)
-        print('Move', start, 'to', end)
-        hanoi(n - 1, via, end, start)
+class Node:
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
 
-# a = []
-# generate(3, 10, a)
-def generate(n, k, finalList, res = 0, first = True):
-    if n == 1:
-        for i in range(k):
-            finalList.append(res + i)
-    else:
-        for i in range(k):
-            if first and i == 0:
-                continue
-            # Korekce kódu ze cvičení: k**(n - 1) místo 10**(n - 1)
-            # např. pro hexadecimální soustavu a dvě číslice korektně dává
-            # čísla od 10(hex)/16(dec) do ff(hex)/255(dec)
-            generate(n - 1, k, finalList, res + i * k**(n - 1), False)
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-# pay(10, [])
-def pay(rem, solution, max = 5):
-    if rem == 0:
-        print(solution)
-    else:
-        for i in [5,2,1]:
-            if rem >= i and i <= max:
-                pay(rem - i, solution + [i], i)
+    def addFront(self, value):
+        self.head = Node(value, self.head)
 
-# permutation([1,2,3], [])
-def permutation(input, cur):
-    if len(input) == 0:
-        print(cur)
-    else:
-        for i, v in enumerate(input):
-            newInput = list(input)
-            del newInput[i]
-            permutation(newInput, cur + [v])
+    def removeFront(self):
+        if self.head != None:
+            self.head = self.head.next
+
+    def _at(self, pos):
+        node = self.head
+        while pos > 0 and node != None:
+            node = node.next
+            pos -= 1
+        return node
+
+    def at(self, pos):
+        node = self._at(pos)
+        if node != None:
+            return node.value
+        else:
+            return None
+
+    def toList(self):
+        result = []
+
+        node = self.head
+        while node != None:
+            result.append(node.value)
+            node = node.next
+
+        return result
+
+    def changeAt(self, pos, value):
+        node = self._at(pos)
+        if node != None:
+            node.value = value
+
+    def insertValue(self, pos, value):
+        prev = None
+        node = self.head
+        while pos > 0 and node != None:
+            prev = node
+            node = node.next
+            pos -= 1
+
+        if pos > 0:
+            return
+
+        newNode = Node(value, node)
+        if prev != None:
+            prev.next = newNode
+        else:
+            self.head = newNode
+
+    def removeValue(self, value):
+        prev = None
+        node = self.head
+        while node != None and node.value != value:
+            prev = node
+            node = node.next
+
+        if node == None:
+            return
+
+        next = node.next
+        if prev != None:
+            prev.next = next
+        else:
+            self.head = next
+
+class DoubleNode:
+    def __init__(self, value, prev = None, next = None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def toList(self):
+        result = []
+        node = self.head
+        while node != None:
+            result.append(node.value)
+            node = node.next
+        return result
+
+    def unlink(self, node):
+        prev = node.prev
+        next = node.next
+
+        node.prev = None
+        node.next = None
+
+        if prev != None:
+            prev.next = next
+        else:
+            self.head = next
+
+        if next != None:
+            next.prev = prev
+        else:
+            self.tail = prev
+
+    def linkAfter(self, prev, node):
+        if prev != None:
+            next = prev.next
+        else:
+            next = self.head
+
+        node.prev = prev
+        node.next = next
+
+        if prev != None:
+            prev.next = node
+        else:
+            self.head = node
+
+        if next != None:
+            next.prev = node
+        else:
+            self.tail = node
+
+    def linkBefore(self, next, node):
+        if next != None:
+            prev = next.prev
+        else:
+            prev = self.tail
+
+        node.prev = prev
+        node.next = next
+
+        if prev != None:
+            prev.next = node
+        else:
+            self.head = node
+
+        if next != None:
+            next.prev = node
+        else:
+            self.tail = node
+
+    def addFront(self, value):
+        node = DoubleNode(value)
+        self.linkAfter(None, node)
+
+    def removeFront(self):
+        self.unlink(self.head)
