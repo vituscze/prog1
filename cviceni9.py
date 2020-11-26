954dedc648518b5e1f62c9b3fd09199e60d17127
Vít Šefl <vituscze@gmail.com>
2020-11-26 16:15:41 +0100

    Update cviceni9.py

--- cviceni9.py
+++ cviceni9.py
@@ -1,71 +1,79 @@
+def parens(opening, closing, string):
+    if opening <= 0 and closing <= 0:
+        print(string)
+    else:
+        if opening > 0:
+            parens(opening - 1, closing + 1, string + '(')
+        if closing > 0:
+            parens(opening, closing - 1, string + ')')
+
+def parensList(opening, closing, string, results):
+    if opening <= 0 and closing <= 0:
+        results.append(string)
+    else:
+        if opening > 0:
+            parensList(opening - 1, closing + 1, string + '(', results)
+        if closing > 0:
+            parensList(opening, closing - 1, string + ')', results)
+
+def hanoi(n, start, end, via):
+    if n == 1:
+        print('Move', start, 'to', end)
+    else:
+        hanoi(n - 1, start, via, end)
+        print('Move', start, 'to', end)
+        hanoi(n - 1, via, end, start)
+
+def pay(amount, coins, solution):
+    if amount == 0:
+        print(*solution)
+    else:
+        for c in coins:
+            if c <= amount:
+                pay(amount - c, coins, solution + [c])
+
+def payUnique(amount, coins, solution):
+    if amount == 0:
+        print(*solution)
+    else:
+        for i, c in enumerate(coins):
+            if c <= amount:
+                payUnique(amount - c, coins[i:], solution + [c])
+
+def permutation(items, result):
+    if len(items) == 0:
+        print(*result)
+    else:
+        for i, v in enumerate(items):
+            newItems = items.copy()
+            del newItems[i]
+            permutation(newItems, result + [v])
+
+def permutationsInPlace(items, rem, result):
+    if rem == 0:
+        print(*result)
+    else:
+        for ix in range(len(items)):
+            if result[ix] != None:
+                continue
+
+            result[ix] = items[-rem]
+            permutationsInPlace(items, rem - 1, result)
+            result[ix] = None
+
 class Node:
-    def __init__(self, value, next = None):
+    def __init__(self, value, left = None, right = None):
         self.value = value
-        self.next = next
+        self.left = left
+        self.right = right
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+def printTree(root):
+    def go(node, depth):
+        if node == None:
+            return
 
-    def addFront(self, value):
-        self.head = Node(value, self.head)
+        go(node.left, depth + 1)
+        print(' ' * 2 * depth + str(node.value))
+        go(node.right, depth + 1)
 
-    def at(self, pos):
-        s = self.head
-        while pos > 0 and s != None:
-            s = s.next
-            pos -= 1
-        if s != None:
-            return s.value
-        else:
-            return None
-
-    def removeFront(self):
-        if self.head != None:
-            self.head = self.head.next
-
-    def removeValue(self, value):
-        prev = None
-        cur = self.head
-        while cur != None and cur.value != value:
-            prev = cur
-            cur = cur.next
-
-        next = None
-        if cur != None:
-            next = cur.next
-
-        if prev != None:
-            prev.next = next
-        else:
-            self.head = next
-
-
-class DoubleNode:
-    def __init__(self, value, prev = None, next = None):
-        self.value = value
-        self.prev = prev
-        self.next = next
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def linkAfter(self, prev, node):
-        next = self.head
-        if prev != None:
-            next = prev.next
-
-        node.prev = prev
-        node.next = next
-
-        if prev != None:
-            prev.next = node
-        else:
-            self.head = node
-
-        if next != None:
-            next.prev = node
-        else:
-            self.tail = node
+    go(root, 0)
