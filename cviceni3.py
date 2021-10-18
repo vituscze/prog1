853671a6a28090e945a5aec340a1694264fdc0fd
Vít Šefl <vituscze@gmail.com>
2021-10-18 22:48:34 +0200

    Update cviceni3.py

--- cviceni3.py
+++ cviceni3.py
@@ -1,20 +1,64 @@
-def sieve(n):
-    if n < 2:
-        return []
+# faktorial
+# n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+# 0! = 1
+def faktorial(n):
+    soucin = 1
+    for i in range(1, n + 1):
+        soucin *= i
+    return soucin
 
-    size = (n - 3) // 2 + 1
-    candidates = size * [True]
-    result = [2]
-    for i in range(size):
-        if candidates[i]:
-            prime = 3 + 2 * i
-            # print('### prime', prime)
-            start = (prime * prime - 3) // 2
-            result.append(prime)
-            for j in range(start, size, 2 * prime // 2):
-                # print('crossing out', 3 + 2 * j)
-                candidates[j] = False
-    return result
+# fibonacci
+# 0 1 1 2 3 5 8...
+# fib(5) = 5
+# fib(0) = 0
+# fib(1) = 1
+def fib(n):
+    if n < 0:
+        return None
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a = 0
+        b = 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
+
+# rostouci podle <=
+# ascending([1,2,3]) == True
+# ascending([1,1,1]) == True
+# ascending([3,2,1]) == False
+def ascending(s):
+    for i in range(len(s) - 1):
+        if s[i] > s[i + 1]:
+            return False
+    return True
+
+def descending(s):
+    for i in range(len(s) - 1):
+        if s[i] < s[i + 1]:
+            return False
+    return True
+
+# setrideny
+# ordered(...)
+def ordered(a):
+    return ascending(a) or descending(a)
+
+# palindrom
+# palindrom('radar') == True
+# palindrom('(())') == False
+def palindrom(s):
+    return s == s[::-1]
+
+def betterPalindrom(s):
+    for i in range(len(s) // 2):
+        if s[i] != s[-1 - i]:
+            return False
+    return True
+
 
 # ----------
 
@@ -46,11 +90,3 @@ while rem > 0:
         print('Player', player, 'won!')
     else:
         player = nextPlayer(player)
-
-# ----------
-
-def palindrom(s):
-    for i in range(len(s) // 2):
-        if s[i] != s[-1 - i]:
-            return False
-    return True
